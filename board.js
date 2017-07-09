401d2cafd7c7b3f863032c81abb9161c7f471826
CandyNinja_ <candyninja001@gmail.com>
2017-07-09 17:06:25 -0400

    Temporarily set all skyfalls to locked

--- board.js
+++ board.js
@@ -71,7 +71,7 @@ function rollOrb() {
   return {
     color:   Math.floor(Math.random() * 6),
     enhanced: false,
-    locked: false,
+    locked: true,
     blind1: false, // Regular blind, slide orbs to reveal
     blind2: 0, // Duration blind, cannot see orbs until timer is over
     matched: false,
